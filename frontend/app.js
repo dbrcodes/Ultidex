44ed04cfefc596eeeca7dea207cf583bf42611d4
dbrcodes <lookitsblizzard@gmail.com>
2025-05-19 22:03:08 +0100

    start on frontend interface

--- frontend/app.js
+++ frontend/app.js
@@ -4,3 +4,22 @@ fetch('http://localhost:5000/api/pokemon/') // backend request
     console.log(data);
     // Render pokémon card here
   });
+
+// page scripts
+const statIds = ['hp', 'attack', 'defense', 'spAtk', 'spDef', 'spd'];
+
+statIds.forEach((id) => {
+  const range = document.getElementById(id);
+  const input = document.getElementById(id + 'Input');
+
+  // When slider changes, update number input
+  range.addEventListener('input', () => {
+    input.value = range.value;
+  });
+
+  // When number input changes, update slider
+  input.addEventListener('input', () => {
+    const val = Math.max(0, Math.min(250, input.value));
+    range.value = val;
+  });
+});
